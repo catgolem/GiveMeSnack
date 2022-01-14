b6ee7f595b4b77ca7e96fee64b3fbb021133ac0a
tosaken1116 <nagato20021122@gmail.com>
2022-01-15 01:10:42 +0900

    [add] #1 Headerの練習

---
+++ components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <header>
+        <h1 class="title">タイトル</h1>
+            <nav class="nav">
+                <ul class="menu-group">
+                    <li class="menu-item"><a href="#">項目1</a></li>
+                    <li class="menu-item"><a href="#">項目2</a></li>
+                    <li class="menu-item"><a href="#">項目3</a></li>
+                    <li class="menu-item"><a href="#">項目4</a></li>
+                    <li class="menu-item"><a href="#">項目5</a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+</template>
+
+<style>
+header {
+  display: flex;
+  width: 100%;
+  height: 100px;
+  background-color: darkgrey;
+  align-items: center;
+}
+ 
+.title {
+  margin-right: auto;
+}
+ 
+.menu-item {
+  list-style: none;
+  display: inline-block;
+  padding: 10px;
+}
+</style>
