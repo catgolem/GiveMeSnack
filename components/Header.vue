b314d03d8c9f835b7477cccc5975e14923d76524
tosaken1116 <nagato20021122@gmail.com>
2022-01-15 15:04:09 +0900

    [modify] #1 headerの実装完了

--- components/Header.vue
+++ components/Header.vue
@@ -1,31 +1,62 @@
 <template>
     <div>
-        <header>
-        <h1 class="title">タイトル</h1>
-            <nav class="nav">
-                <ul class="menu-group">
-                    <li class="menu-item"><a href="#">項目1</a></li>
-                    <li class="menu-item"><a href="#">項目2</a></li>
-                    <li class="menu-item"><a href="#">項目3</a></li>
-                    <li class="menu-item"><a href="#">項目4</a></li>
-                    <li class="menu-item"><a href="#">項目5</a></li>
-                </ul>
-            </nav>
+        <header class="text-black">
+          <h1 class="title">GiveMeSnack</h1>
+          <nav class="nav" v-if = this.work.situation >勤務中</nav>
+          <nav class="nav" v-else>休憩中</nav>
         </header>
     </div>
 </template>
+
+<script>
+export default {
+  setup() {
+  },
+  data(){
+    return {
+      work: {
+        now_time_str:'',
+        rest_time:50,
+        now_time:0,
+        situation:true,
+      },
+      show: false,
+    }
+  },
+  methods: {
+    async get_situation(){
+      var d = new Date();
+      var mm = ('0' + d.getMinutes()).slice(-2);
+      this.work.now_time_str =mm;
+      this.work.now_time = Number(this.work.now_time_str);
+      if(this.work.now_time >= this.work.rest_time){
+        this.work.situation = false;
+      }
+    }
+  },
+
+  async mounted(){
+    await this.get_situation();
+  },
+}
+</script>
 
 <style>
 header {
   display: flex;
   width: 100%;
   height: 100px;
-  background-color: darkgrey;
+  background-color: white;
   align-items: center;
 }
  
 .title {
-  margin-right: auto;
+  margin:5%
+}
+
+.nav {
+  margin:10%;
+
 }
  
 .menu-item {
