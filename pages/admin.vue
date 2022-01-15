9bf979e880da7b6bad1a0c8d62c250d15615a587
tosaken1116 <nagato20021122@gmail.com>
2022-01-15 20:47:56 +0900

    [modify] #6 当番表の作成をしました

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
     <Header/>
-    <div class="ml-16" >
-        <h1 class="mt-10 font-black text-2xl">今週買ったお菓子</h1>
-        <h2 class="h2 mt-6"> 
-            <input type="text" autocomplete="on" v-model="snack_name" class="shadow-md rounded-lg h21 pl-2 py-1 w-44" placeholder="入力"/>
-            <button class="hover:bg-black shadow-md button ml-3 rounded-lg text-white bg-gray-400 p-1" @click="postSnack()">追加</button>
-        </h2>
+    <div class="submit">
+        <div class="left ml-16" >
+            <h1 class="mt-10 font-black text-2xl">今週買ったお菓子</h1>
+            <h2 class="h2 mt-6"> 
+                <input type="text" autocomplete="on" v-model="snack_name" class="shadow-md rounded-lg h21 pl-2 py-1 w-44" placeholder="入力"/>
+                <button class="shadow-md ml-3 rounded-lg text-white bg-gray-400 p-1" @click="postSnack()">追加</button>
+            </h2>
+        </div>
+        <div class="right ml-10 mr-16 mt-12 mb-5 bg-gray-200 p-3 rounded-lg w-80 shadow-md">
+            <div>
+                <h11 class="border-b-2 border-gray-400">当番表</h11>
+                <h12 class="ml-5">今週の当番は</h12>
+            </div>
+            <div class="m-7">
+                    <div class="ml-5">
+                    <div class="mb-3">１．イヌさん</div>
+                    <div class="mb-3">２．ハトさん</div>
+                    <div class="mb-3">３．ネコさん</div>
+                    <div class="mb-3">４．ドジョウさん</div>
+                    <div class="mb-3">５．ヤギさん</div>
+                    <div class="mb-3">６．フライゴンさん</div>
+                </div>
+                <div class="text-center">
+                    <button class="shadow-md bg-white rounded-full text-black mx-auton">編集</button>
+                </div>
+            </div>
+        </div>
     </div>
   </div>
 </template>
@@ -40,5 +61,11 @@ export default{
     border-radius: 5px;
     border: solid;
 }
-
+.submit{
+    display: flex;
+    flex-wrap: wrap;
+}
+.button{
+    text-align: center;
+}
 </style>
