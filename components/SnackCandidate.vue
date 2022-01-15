3f12ed255e5840fcc56d5b34935375a9a6ae200d
uktallyho <kokiuaxdx@gmail.com>
2022-01-16 00:01:48 +0900

    [add] #3 お菓子提案フォームなどの作成

---
+++ components/SnackCandidate.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="" class="">
+    <div class="snack-vote-element flex">
+      <div class="snack-vote-list flex-1 leading-8">
+        ・
+        <span id="snack-vote-name" class="snack-vote-name"></span>
+      </div>
+      <div class="snack-like flex-1">
+        <button class="like-button">
+          <img src="../assets/photo/round_thumb_up_alt_black_24dp.png" alt="" class="p-2" style="width: 2rem; height: 2rem; display: inline;">
+        </button>
+        <span id="snack-like-num" class="snack-like-num"></span>
+      </div>
+    </div>
+  </div>
+</template>
