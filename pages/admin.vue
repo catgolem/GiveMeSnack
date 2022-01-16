0a4c7c1b55e0706bf57856342eae634cf504e66e
ankon64 <ankonhouse@gmail.com>
2022-01-16 12:47:49 +0900

    [modify] adminのカラー設定

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
+  <div class="background-rd">
     <Header/>
     <div class="submit">
         <div class="left ml-16" >
-            <h1 class="mt-10 font-black text-2xl">今週買ったお菓子</h1>
+            <h1 class="mt-10 font-black text-2xl rounded" style="background: rgb(252 222 108)">今週買ったお菓子</h1>
             <h2 class="h2 mt-6"> 
                 <input type="text" autocomplete="on" v-model="snack_name" class="shadow-md rounded-lg h21 pl-2 py-1 w-44" placeholder="入力"/>
-                <button class="shadow-md ml-3 rounded-lg text-white bg-gray-400 p-1" @click="postSnack()">追加</button>
+                <button class="shadow-md ml-3 rounded-lg text-white bg-choco p-1" @click="postSnack()">追加</button>
             </h2>
         </div>
-        <div class="right ml-10 mr-16 mt-12 mb-5 bg-gray-200 p-3 rounded-lg w-80 shadow-md">
+        <div class="right ml-10 mr-16 mt-12 mb-5 background-white p-3 w-80 shadow-md">
             <div>
                 <h11 class="border-b-2 border-gray-400">当番表</h11>
                 <h12 class="ml-5">今週の当番は</h12>
@@ -68,4 +68,16 @@ export default{
 .button{
     text-align: center;
 }
+.background-rd {
+    background: repeating-linear-gradient(-45deg, #d0546a22, #d0546a22 40px, #d0546a 40px, #d0546a 80px);
+    min-height: 100vh;
+}
+.background-white{
+  border-radius: 20px;
+  background: #f3f3f3;
+  box-sizing: border-box;
+  border: solid 24px #e0e0e0;
+  border-top: solid 24px #ccc;
+  border-bottom: solid 24px #fff;
+} 
 </style>
